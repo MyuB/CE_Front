915e9dc3403b264766feb894cc4ab5142bc11404
MyuB <mjycoco0@naver.com>
2023-03-25 12:50:01 +0900

    feat: 탄소발자국을 직접 계산해주는 하위 모델에 대한 feat 완성

--- src/Components/trafficCarbon.jsx
+++ src/Components/trafficCarbon.jsx
@@ -1,12 +1,22 @@
-function TrafficCarbon(props) {
+function TrafficCarbon({ busTime, subwayTime }) {
   const busCarbon = (time) => {
-    return time * 0.006;
-  };
-  const subwayCarbon = (time) => {
-    return ((time * 0.424 * 34) / 60) * 0.000182;
+    const t = parseInt(time);
+    return t * 0.006;
   };
 
-  return <div></div>;
+  const subwayCarbon = (time) => {
+    const t = parseInt(time);
+    return ((t * 0.424 * 34) / 60) * 0.000182;
+  };
+  const totalCarbonFootprint = () => {
+    return busCarbon(busTime) + subwayCarbon(subwayTime);
+  };
+
+  return (
+    <div>
+      교통에서 소비된 탄소발자국은 {totalCarbonFootprint()}㎏-CO2 입니다.
+    </div>
+  );
 }
 
 export default TrafficCarbon;
