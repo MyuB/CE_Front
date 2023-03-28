41abbbc293d56d5ad2ad0c5424cbf053bf067556
MyuB <mjycoco0@naver.com>
2023-03-28 20:18:26 +0900

    feat: group entry component에서 router 설정

--- src/Components/Group/group.jsx
+++ src/Components/Group/group.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 import "./group.scss";
 
 const GroupButton = styled.div`
@@ -14,10 +15,18 @@ const GroupButton = styled.div`
 `;
 
 function Group() {
+  const navigate = useNavigate();
+
+  const createGroup = () => {
+    navigate("/createGroup");
+  };
+  const joinGroup = () => {
+    navigate("/joinGroup");
+  };
   return (
     <div className={"button-wrapper"}>
-      <GroupButton>{"그룹만들기"}</GroupButton>
-      <GroupButton>{"그룹참여하기"}</GroupButton>
+      <GroupButton onClick={createGroup}>{"그룹만들기"}</GroupButton>
+      <GroupButton onClick={joinGroup}>{"그룹참여하기"}</GroupButton>
     </div>
   );
 }
