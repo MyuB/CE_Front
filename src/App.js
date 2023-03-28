bcd59677eee500194e32aea592bb0694f30395b5
MyuB <mjycoco0@naver.com>
2023-03-28 19:58:12 +0900

    feat: react-router-dom을 활용한 router설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import Group from "./Components/Group/group";
+import Traffic from "./Components/Traffic/traffic";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/reset.css";
+import NotFound from "./Components/NotFound/notFound";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NotFound />} />
+          <Route path="/traffic" element={<Traffic />} />
+          <Route path="/group" element={<Group />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
