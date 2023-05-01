e9aac01e6c9c2e8a69c50198ff74dca93de52cd7
MyuB <mjycoco0@naver.com>
2023-05-01 22:28:42 +0900

    feat: group관련 API 생성

---
+++ src/API/group.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { getToken } from "utils/localStorage";
+
+export const groupCreate = () => {
+  const tokens = getToken();
+  if (!tokens) alert("no tokens valid!");
+
+  return axios
+    .post(
+      `${process.env.REACT_APP_BASEURL}/group`,
+      {},
+      {
+        headers: {
+          accessToken: tokens.accessToken,
+          refreshToken: tokens.refreshToken,
+        },
+      }
+    )
+    .then((res) => res.data);
+};
+
+export const getRank = (groupd_index) => {
+  const tokens = getToken();
+  if (!tokens) alert("no tokens valid!");
+
+  return axios
+    .get(`${process.env.REACT_APP_BASEURL}/group`, {
+      params: { groupd_index },
+      headers: {
+        accessToken: tokens.accessToken,
+        refreshToken: tokens.refreshToken,
+      },
+    })
+    .then((res) => res.data);
+};
