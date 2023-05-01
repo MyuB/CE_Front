4648822045b809977e4bee4bb9cf85b624bfaf2d
MyuB <mjycoco0@naver.com>
2023-05-01 22:29:10 +0900

    feat: traffic관련 API 작성

---
+++ src/API/traffic.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { getToken } from "utils/localStorage";
+
+export const setTrafficCarbon = (carbon) => {
+  const tokens = getToken();
+
+  return axios
+    .post(
+      `${process.env.REACT_APP_BASEURL}/traffic`,
+      { carbon },
+      {
+        headers: {
+          Authorization: tokens.accessToken,
+        },
+      }
+    )
+    .then((res) => res.data);
+};
+
+export const getTrafficSolution = () => {
+  const tokens = getToken();
+
+  return axios
+    .get(`${process.env.REACT_APP_BASEURL}/traffic`, {
+      headers: {
+        Authorization: tokens.accessToken,
+      },
+    })
+    .then((res) => res.data);
+};
+
+export const getTrafficCarbon = () => {
+  const tokens = getToken();
+
+  return axios
+    .get(`${process.env.REACT_APP_BASEURL}/traffic/carbon`, {
+      headers: {
+        Authorization: tokens.accessToken,
+      },
+    })
+    .then((res) => res.data);
+};
