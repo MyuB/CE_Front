622e772fda518f6ded559d6ed577feb521cdad8e
MyuB <mjycoco0@naver.com>
2023-05-11 17:25:49 +0900

    feat: group Member Component 생성

---
+++ src/Components/GroupMember/Member.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+function Member() {
+  return (
+    <React.Fragment>
+      <div></div>
+    </React.Fragment>
+  );
+}
+
+export default Member;
