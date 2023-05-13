4ac1f9c2f3d8dc2e9dae15b7f10f26cb9ce5ed3a
MyuB <mjycoco0@naver.com>
2023-05-13 15:54:31 +0900

    feat: 피그마 명세대로 가운데에 있는 Div Box 생성

--- src/Components/GroupMember/Member.jsx
+++ src/Components/GroupMember/Member.jsx
@@ -1,10 +1,50 @@
 import React from "react";
 import styled from "styled-components";
 
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+`;
+
+const BoxWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  border-radius: 2vh;
+  background-color: #009688;
+  height: 60vh;
+  width: 80vw;
+  margin-top: 10vh;
+`;
+
+const BarsWrapper = styled.div`
+  height: 30vh;
+  width: 70vw;
+`;
+
+const Circle = styled.div`
+  border-radius: 50%;
+  background-color: #d9d9d9;
+  text-align: center;
+  height: 25vh;
+  width: 25vh;
+  line-height: 25vh;
+  font-size: 5vh;
+`;
+
 function Member() {
   return (
     <React.Fragment>
-      <div></div>
+      <MainWrapper>
+        <BoxWrapper>
+          <BarsWrapper></BarsWrapper>
+          <Circle>1</Circle>
+        </BoxWrapper>
+      </MainWrapper>
     </React.Fragment>
   );
 }
