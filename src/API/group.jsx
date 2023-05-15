dbe8d539ebc58ab1b8f35124e28cbc01f02b7bf7
MyuB <mjycoco0@naver.com>
2023-05-15 18:02:58 +0900

    fix: api 명세서에 따른 API 호출 메서드 수정

--- src/API/group.jsx
+++ src/API/group.jsx
@@ -1,14 +1,26 @@
 import axios from "axios";
 import { getToken } from "utils/localStorage";
 
-export const groupCreate = () => {
+export const groupCreate = (
+  group_name,
+  start_time,
+  end_time,
+  is_food,
+  is_traffic
+) => {
   const tokens = getToken();
   if (!tokens) alert("no tokens valid!");
 
   return axios
     .post(
       `${process.env.REACT_APP_BASEURL}/group`,
-      {},
+      {
+        group_name,
+        start_time,
+        end_time,
+        is_food,
+        is_traffic,
+      },
       {
         headers: {
           Authorization: tokens.accessToken,
@@ -16,6 +28,23 @@ export const groupCreate = () => {
       }
     )
     .then((res) => res.data);
+};
+
+export const joinGourp = (inviteCode) => {
+  const tokens = getToken();
+  if (!tokens) alert("no tokens valid!");
+
+  return axios.post(
+    `${process.env.REACT_APP_BASEURL}/group/join`,
+    {
+      invite_code: inviteCode,
+    },
+    {
+      headers: {
+        Authorization: tokens.accessToken,
+      },
+    }
+  );
 };
 
 export const getRank = (group_index) => {
