f3863a50626cecfd2f9e8b358e85cce547ba950f
MyuB <mjycoco0@naver.com>
2023-05-22 23:57:09 +0900

    refactor: 피그마 명세대로 group 페이지에서의 refactoring

--- src/Components/Group/group.jsx
+++ src/Components/Group/group.jsx
@@ -3,15 +3,36 @@ import { useNavigate } from "react-router-dom";
 import "./group.scss";
 
 const GroupButton = styled.div`
-  background-color: #009688;
+  background-color: #92b8b1;
   color: white;
-  font-size: 4vh;
-  width: 25vh;
-  height: 12vh;
+  font-size: 2.5vh;
+  width: 40vh;
+  height: 8vh;
   text-align: center;
-  line-height: 12vh;
-  margin-bottom: 5vh;
+  line-height: 8vh;
+  margin-bottom: 3vh;
   border-radius: 10px;
+  font-weight: bold;
+`;
+
+const WhiteGroupButton = styled.div`
+  background-color: #fff;
+  color: #009688;
+  font-size: 2.5vh;
+  width: 40vh;
+  height: 8vh;
+  text-align: center;
+  line-height: 8vh;
+  margin-bottom: 3vh;
+  border-radius: 10px;
+  font-weight: bold;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 function Group() {
@@ -28,9 +49,15 @@ function Group() {
   };
   return (
     <div className={"button-wrapper"}>
-      <GroupButton onClick={createGroup}>{"그룹만들기"}</GroupButton>
-      <GroupButton onClick={joinGroup}>{"그룹참여하기"}</GroupButton>
-      <GroupButton onClick={rankGroup}>{"내 그룹으로 가기"}</GroupButton>
+      <div className={"top_header"}>{"Group"}</div>
+      <ButtonWrapper>
+        {" "}
+        <GroupButton onClick={createGroup}>{"그룹만들기"}</GroupButton>
+        <WhiteGroupButton onClick={joinGroup}>
+          {"그룹참여하기"}
+        </WhiteGroupButton>
+        <GroupButton onClick={rankGroup}>{"내 그룹"}</GroupButton>
+      </ButtonWrapper>
     </div>
   );
 }
