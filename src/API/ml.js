62ceac1b9f3c44a33374f23d7f441049ac34c447
MyuB <mjycoco0@naver.com>
2023-05-30 18:05:29 +0900

    feat: 머신러닝을 위한 api 호출 메서드 작성

--- src/API/ml.js
+++ src/API/ml.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const getPrediction = (formData) => {
+  console.log(formData);
+
   return axios
     .post(`${process.env.REACT_APP_PYTHON}`, formData)
     .then((res) => res.data);
